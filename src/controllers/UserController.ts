b7a19f3697b2ca2795d98a4b8e71dc56d423af26
DarknessNinjaBR <darkenessninjabr@hotmail.com>
2021-03-11 19:13:47 -0300

    Error messages refactory

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import { FindOperator, getCustomRepository } from "typeorm";
 import { UsersRepository } from "../repositories/UsersRepository";
 import * as yup from 'yup';
+import { AppError } from "../errors/AppError";
 
 class UserController{
     async create(request: Request, response: Response){
@@ -15,7 +16,8 @@ class UserController{
         try{
             await schema.validate(request.body,  { abortEarly: false})
         }catch(err){
-            return response.status(400).json({error: err});
+            throw new AppError(err);
+
         }
 
         const usersRepository = getCustomRepository(UsersRepository);
@@ -25,9 +27,7 @@ class UserController{
         });
 
         if(userAlreadyExists){
-            return response.status(400).json({
-                error: "User already existis",
-            });
+            throw new AppError("User already existis");
         }
 
         
